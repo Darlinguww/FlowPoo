6cb508ca1d37742d50f30472192a29ae2dcf5509
Jose M <111933458+Znake-G@users.noreply.github.com>
2023-11-29 05:20:54 -0500

    Final Gerente & Supervisor Txt

--- Gerente/app.py
+++ Gerente/app.py
@@ -1,192 +1,64 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3 as sql
 
 app = Flask(__name__)
 
-# Lógica para crear la tabla en la base de datos
-def create_table():
-    try:
-        # Conectar a la base de datos
-        con = sql.connect("Gerente/Salarios.db")
-        
-        # Crear un objeto cursor para ejecutar consultas
-        cursor = con.cursor()
+def get_supervisors():
+    supervisors = []
+    with open('supervisores.txt', 'r') as file:
+        for line in file:
+            data = line.split('/')
+            supervisors.append([data[0], data[1], data[3], data[4]])
+    return supervisors
 
-        # Crear la tabla si no existe
-        cursor.execute("""CREATE TABLE IF NOT EXISTS usuarios (
-                          Name TEXT,
-                          Salary INTEGER)""")
-        
-        # Guardar los cambios y cerrar la conexión
-        con.commit()
-        con.close()
-        
-        print("Tabla creada correctamente.")
-    except sql.Error as e:
-        print("Error en la base de datos:", e)
-
-# Llamada a la función para crear la tabla
-create_table()
-
-# Lógica para actualizar la base de datos
-def update_database(supervisor_name, new_salary):
-    try:
-        # Conectar a la base de datos
-        con = sql.connect("Gerente/Salarios.db")
-
-        # Crear un objeto cursor para ejecutar consultas
-        cursor = con.cursor()
-
-        # Ejecutar la actualización
-        cursor.execute("UPDATE usuarios SET Salary = ? WHERE Name = ?", (new_salary, supervisor_name))
-
-        # Guardar los cambios y cerrar la conexión
-        con.commit()
-        con.close()
-
-        print("Salario actualizado correctamente.")
-    except sql.Error as e:
-        print("Error en la base de datos:", e)
-
-# Lógica para agregar un supervisor a la base de datos
-def add_supervisor(name, salary):
-    try:
-        # Conectar a la base de datos
-        con = sql.connect("Gerente/Salarios.db")
-
-        # Crear un objeto cursor para ejecutar consultas
-        cursor = con.cursor()
-
-        # Ejecutar la inserción
-        cursor.execute("INSERT INTO usuarios (Name, Salary) VALUES (?, ?)", (name, salary))
-
-        # Guardar los cambios y cerrar la conexión
-        con.commit()
-        con.close()
-
-        print("Supervisor agregado correctamente.")
-    except sql.Error as e:
-        print("Error en la base de datos:", e)
-
-# Lógica para eliminar un supervisor de la base de datos
-def delete_supervisor(name):
-    try:
-        # Conectar a la base de datos
-        con = sql.connect("Gerente/Salarios.db")
-
-        # Crear un objeto cursor para ejecutar consultas
-        cursor = con.cursor()
-
-        # Ejecutar la eliminación
-        cursor.execute("DELETE FROM usuarios WHERE Name = ?", (name,))
-
-        # Guardar los cambios y cerrar la conexión
-        con.commit()
-        con.close()
-
-        print("Supervisor eliminado correctamente.")
-    except sql.Error as e:
-        print("Error en la base de datos:", e)
-
-# Ruta para la página principal
 @app.route('/')
 def index():
-    try:
-        # Conectar a la base de datos
-        con = sql.connect("Gerente/Salarios.db")
+    return render_template('index_Gerente.html')
 
-        # Crear un objeto cursor para ejecutar consultas
-        cursor = con.cursor()
-
-        # Obtener los datos de los supervisores
-        cursor.execute("SELECT Name, Salary FROM usuarios")
-        supervisors_data = cursor.fetchall()
-
-        # Cerrar la conexión
-        con.close()
-
-        return render_template('index_Gerente.html', supervisors=supervisors_data)
-
-    except sql.Error as e:
-        print("Error en la base de datos:", e)
-        return "Error en la base de datos"
-
-
-# Ruta para la actualización de salario
-@app.route('/update_salary', methods=['POST'])
-def update_salary():
-    supervisor_name = request.form.get('name')
-    new_salary = request.form.get('new_salary')
-
-    # Llamada a la función para actualizar la base de datos
-    update_database(supervisor_name, new_salary)
-
-    # Redireccionar a la página principal después de la actualización
-    return redirect(url_for('index'))
-
-
-# Ruta para agregar un supervisor
-@app.route('/add_supervisor', methods=['POST'])
-def add_supervisor_route():
-    name = request.form.get('name')
-    salary = request.form.get('salary')
-
-    # Llamada a la función para agregar un supervisor a la base de datos
-    add_supervisor(name, salary)
-
-    # Redireccionar a la página principal después de agregar el supervisor
-    return redirect(url_for('index'))
-
-
-# Ruta para eliminar un supervisor
-@app.route('/delete_supervisor', methods=['POST'])
-def delete_supervisor_route():
-    supervisor_name = request.form.get('name')
-
-    # Llamada a la función para eliminar un supervisor de la base de datos
-    delete_supervisor(supervisor_name)
-
-    # Redireccionar a la página principal después de eliminar el supervisor
-    return redirect(url_for('index'))
-
-
-
-# Ruta para la página de salarios
-@app.route('/salarios')
-def salarios():
-    try:
-        # Conectar a la base de datos
-        con = sql.connect("Gerente/Salarios.db")
-
-        # Crear un objeto cursor para ejecutar consultas
-        cursor = con.cursor()
-
-        # Obtener los datos de los supervisores
-        cursor.execute("SELECT Name, Salary FROM usuarios")
-        supervisors_data = cursor.fetchall()
-
-        # Imprime los datos en la consola para verificar
-        print("Supervisors Data:", supervisors_data)
-
-        # Cerrar la conexión
-        con.close()
-
-        return render_template('Salarios.html', supervisors=supervisors_data)
-
-    except sql.Error as e:
-        print("Error en la base de datos:", e)
-        return "Error en la base de datos"
-
-
-
-@app.route('/add_super')
-def add_super():
-    return render_template('Add_Super.html')
-
-@app.route('/delete_super')
+@app.route('/delete_super', methods=['GET', 'POST'])
 def delete_super():
+    if request.method == 'POST':
+        name = request.form['name']
+        with open('supervisores.txt', 'r') as file:
+            lines = file.readlines()
+        with open('supervisores.txt', 'w') as file:
+            for line in lines:
+                if not line.startswith(f"{name}/"):
+                    file.write(line)
+        # Redirige nuevamente a la página de eliminación después de la operación
+        return redirect(url_for('delete_super'))
+
+    # Si la solicitud es GET, simplemente renderiza la plantilla de eliminación
     return render_template('Delete_Super.html')
 
-# Iniciar la aplicación Flask
+@app.route('/add_super', methods=['GET', 'POST'])
+def add_super():
+    if request.method == 'POST':
+        name = request.form['name']
+        salary = request.form['salary']
+        with open('supervisores.txt', 'a') as file:
+            file.write(f"{name}/n{name}@example.com/contraseña/{salary}/Supervisor\n")
+        return redirect(url_for('index'))
+
+    return render_template('Add_Super.html')
+
+@app.route('/salaries', methods=['GET', 'POST'])
+def manage_salaries():
+    if request.method == 'POST':
+        name_to_update = request.form.get('name_to_update')
+        new_salary = request.form.get('new_salary')
+        with open('supervisores.txt', 'r') as file:
+            lines = file.readlines()
+        with open('supervisores.txt', 'w') as file:
+            for line in lines:
+                if line.startswith(f"{name_to_update}/"):
+                    updated_line = f"{name_to_update}/{line.split('/')[1]}/{line.split('/')[2]}/{new_salary}/{line.split('/')[4]}"
+                    file.write(updated_line)
+                else:
+                    file.write(line)
+        return redirect(url_for('manage_salaries'))
+
+    supervisors = get_supervisors()
+    return render_template('Salarios.html', supervisors=supervisors)
+
 if __name__ == '__main__':
     app.run(debug=True)
